e3db34c5e2b4470b06345fbc583499446d915bbe
wjrcode <willianjunior012@gmail.com>
2025-03-15 20:25:37 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<script setup>
+import NavigationBar from './NavigationBar.vue'
+</script>
+
+<template>
+  <NavigationBar />
+  <div class="product">
+    <img class="product__image" src="./assets/image-product-1.jpg" />
+    <header>
+      <p>Sneaker Copany</p>
+      <h1>Fall Limited Edition Sneakers</h1>
+    </header>
+    <p>
+      These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber
+      outer sole, they’ll withstand everything the weather can offer.
+    </p>
+    <div class="product__pricing">
+      <h2>$125.00</h2>
+      <span class="product__discount">50%</span>
+      <p>$250.00</p>
+    </div>
+  </div>
+
+  <button class="product__button"><img src="./assets/icon-cart.svg" />Add to cart</button>
+</template>
+
+<style>
+:root {
+  font-family: Kumbh Sans;
+}
+
+body {
+  margin: 0;
+}
+
+.product__image {
+  height: 360px;
+  width: 100%;
+  object-fit: cover;
+}
+
+.product__pricing {
+  display: flex;
+  align-items: center;
+}
+
+.product__discount {
+  background-color: black;
+  border-radius: 0.5rem;
+  color: white;
+  font-weight: 700;
+  padding: 0.5rem;
+}
+
+.product__button {
+  width: 100%;
+  background-color: hsl(26, 100%, 55%);
+  border: none;
+  padding: 1.5rem;
+  border-radius: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  color: black;
+  font-weight: 700;
+  font-size: 1.5rem;
+}
+</style>
---
+++ src/NavigationBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="nav">
+    <div>
+      <img src="./assets/icon-menu.svg" alt="Menu Icon" class="nav__menu" />
+      <img src="./assets/logo.svg" alt="Logo Sneakers" />
+    </div>
+    <div>
+      <img src="./assets/icon-cart.svg" alt="Cart Icon" />
+      <img class="nav-avatar" src="./assets/image-avatar.png" />
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.nav {
+  padding: 1rem 2rem;
+  display: flex;
+  justify-content: space-between;
+  background-color: blanchedalmond;
+}
+
+.nav__menu {
+  margin-right: 1rem;
+}
+
+.nav-avatar {
+  width: 24px;
+  margin-left: 1rem;
+}
+</style>
